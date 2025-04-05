df4e28f0b9c9c9a887660bf254015e0119d60e9b
CipherCrow <jonastorquatro@gmail.com>
2025-04-04 23:47:21 -0300

    iniciando projeto

---
+++ src/main/java/br/com/hackaton/priorizasus/PriorizaSusApplication.java
@@ -0,0 +1,13 @@
+package br.com.hackaton.priorizasus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriorizaSusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriorizaSusApplication.class, args);
+	}
+
+}
