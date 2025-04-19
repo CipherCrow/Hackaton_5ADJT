cf41f16fb85a285d1770961a8fad1dea448ca945
CipherCrow <jonastorquatro@gmail.com>
2025-04-19 18:18:58 -0300

    Correção de validação

--- src/main/java/br/com/hackaton/priorizasus/repository/FilaAtendimentoRepository.java
+++ src/main/java/br/com/hackaton/priorizasus/repository/FilaAtendimentoRepository.java
@@ -12,5 +12,5 @@ import java.util.Optional;
 public interface FilaAtendimentoRepository extends JpaRepository<FilaAtendimento, Long> {
 
     List<FilaAtendimento> findByStatusAtendimentoEnum(StatusAtendimentoEnum statusAtendimento);
-    Optional<FilaAtendimento> findByTriagem_Paciente_Cpf(String cpf);
+    Optional<FilaAtendimento> findByTriagem_Paciente_CpfAAndStatusAtendimentoEnum(String cpf,StatusAtendimentoEnum statusAtendimento);
 }
